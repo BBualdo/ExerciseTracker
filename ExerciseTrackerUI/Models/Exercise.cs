180225b514ded01a56d6c2beb3f72ef93fc499ec
Sebastian <mrseb10@gmail.com>
2024-04-21 20:41:22 +0200

    UI (based on ShiftsLogger UI)

---
+++ ExerciseTrackerUI/Models/Exercise.cs
@@ -0,0 +1,11 @@
+﻿namespace ExerciseTrackerUI.Models;
+
+internal class Exercise
+{
+  public int Id { get; set; }
+  public string Name { get; set; } = "Exercise";
+  public DateTime StartDate { get; set; }
+  public DateTime EndDate { get; set; }
+  public TimeSpan Duration => EndDate - StartDate;
+  public string? Comments { get; set; }
+}
